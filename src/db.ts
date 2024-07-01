5339fb17e8a790ad5c29c2fff3ebab86c7de18a9
steinhardt21 <alexandru.enache@protonmail.com>
2024-07-01 16:45:54 +0300

    refactor: Update findNearestStationsWithinRadiusForCompany function to use radiusMeters variable

--- src/db.ts
+++ src/db.ts
@@ -6,24 +6,25 @@ export const prisma = new PrismaClient().$extends({
   model: {
     station: {
       async findNearestStationsWithinRadiusForCompany({companyId, latitude, longitude, radiusKilometers}: SearchQuery): Promise<StationModelDB[]> {
+        const radiusMeters = Number(radiusKilometers) * 1000
         const result = await prisma.$queryRaw<
         StationModelDB[]
         > `
          WITH RECURSIVE company_hierarchy AS (
-          SELECT id, name, parent-id
+          SELECT id, name, parent_id
           FROM "Company"
           WHERE id = ${companyId}
           UNION ALL
-          SELECT c.id, c.name, c.parent-id
+          SELECT c.id, c.name, c.parent_id
           FROM "Company" c
-          INNER JOIN company_hierarchy ch ON c.parent-id = ch.id
+          INNER JOIN company_hierarchy ch ON c.parent_id = ch.id
         )
         SELECT s.*, ROUND(earth_distance(ll_to_earth(${Number(latitude)},${Number(longitude)}), ll_to_earth(latitude, longitude))::NUMERIC, 2) AS distance
         FROM "Station" AS s
         JOIN company_hierarchy ch ON s.company_id = ch.id
         WHERE
-        earth_box(ll_to_earth (${Number(latitude)},${Number(longitude)}), ${Number(radiusKilometers)}) @> ll_to_earth (latitude, longitude)
-        AND earth_distance(ll_to_earth (${Number(latitude)},${Number(longitude)}), ll_to_earth (latitude, longitude)) < ${Number(radiusKilometers)}
+        earth_box(ll_to_earth (${Number(latitude)},${Number(longitude)}), ${radiusMeters}) @> ll_to_earth (latitude, longitude)
+        AND earth_distance(ll_to_earth (${Number(latitude)},${Number(longitude)}), ll_to_earth (latitude, longitude)) < ${radiusMeters}
         ORDER BY
         distance;`
 
